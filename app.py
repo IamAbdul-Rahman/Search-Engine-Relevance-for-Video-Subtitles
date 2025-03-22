2fc42706e6d59e1b7c77de1676dc00a3337544dc
Abdul Rahman <91885015+IamAbdul-Rahman@users.noreply.github.com>
2025-03-23 04:05:08 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import tempfile
 import os
 
 
-st.title("🎥 SubSearch AI")
+st.title("🎬 Video Subtitle Search Engine")
 st.subheader("Subtitle Search Engine Powered by NLP & ML")
 
 uploaded_file = st.file_uploader("Upload a 4-5 min audio clip (MP3 or WAV)", type=["mp3", "wav"])
